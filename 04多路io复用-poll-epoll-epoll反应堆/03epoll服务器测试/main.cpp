3f69210414d44f0f9993d38c630be305f6d6a54d
haifengMC <1745502795@qq.com>
2019-08-26 19:06:32 +0800

    epoll服务器初步实现

--- 04多路io复用-poll-epoll-epoll反应堆/03epoll服务器测试/main.cpp
+++ 04多路io复用-poll-epoll-epoll反应堆/03epoll服务器测试/main.cpp
@@ -2,7 +2,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
-#include <vector>
+#include <list>
 #include <iostream>
 
 #define PORT 8080
@@ -13,19 +13,23 @@ int main()
 {
 	epoll_event event, *events;
 	int size = 0, capacity = 32;
-	vector<int> vctrFd;
+	list<int> listFd;
 	event.events = EPOLLIN;
 
 	int epollFd = epoll_create(32);
 
 	event.data.fd = STDIN_FILENO;
 	epoll_ctl(epollFd, EPOLL_CTL_ADD, STDIN_FILENO, &event);
+	//listFd.push_back(STDIN_FILENO);
 	++size;
 
 	int lstnFd = socket(AF_INET, SOCK_STREAM, 0);
+	int opt = 1;
+	setsockopt(lstnFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
 	event.data.fd = lstnFd;
 	epoll_ctl(epollFd, EPOLL_CTL_ADD, lstnFd, &event);
+	//vctrFd.push_back(lstnFd);
 	++size;
 
 	sockaddr_in sa;
@@ -59,7 +63,7 @@ int main()
 					fgets(buf, sizeof(buf) - 1, stdin);
 					if ('\n' == buf[strlen(buf) - 1])
 						buf[strlen(buf) - 1] = 0;
-					for (int fd : vctrFd)
+					for (int fd : listFd)
 					{
 						send(fd, buf, strlen(buf) + 1, 0);
 					}
@@ -70,13 +74,35 @@ int main()
 					sockaddr_in sa;
 					socklen_t saLen;
 					int connFd = accept(lstnFd, (sockaddr*)&sa, &saLen);
+					listFd.push_back(connFd);
+					event.data.fd = connFd;
+					epoll_ctl(epollFd, EPOLL_CTL_ADD, connFd, &event);
+					++size;
 					inet_ntop(AF_INET, &sa, bufIP, saLen);
-					cout << bufIP << ":" << ntohs(sa.sin_port) << endl;
-					vctrFd.push_back(connFd);
+					cout << "[" << listFd.size() << "]" << bufIP << ":" << ntohs(sa.sin_port) << endl;
 				}
 				else
 				{
-
+					char buf[64] = { 0 };
+					int n = recv(events[i].data.fd, buf, sizeof(buf) - 1, 0);
+					switch (n)
+					{
+					case -1:
+						perror("recv err");
+						goto err1;
+					case 0:
+						cout << "close connect" << endl;
+						listFd.remove(events[i].data.fd);
+						event.data.fd = events[i].data.fd;
+						epoll_ctl(epollFd, EPOLL_CTL_DEL, events[i].data.fd, &event);
+						--size;
+						break;
+					default:
+						if ('\n' == buf[n - 1])
+							buf[n - 1] = 0;
+						cout << buf << endl;
+						break;
+					}
 				}
 			}
 			break;
