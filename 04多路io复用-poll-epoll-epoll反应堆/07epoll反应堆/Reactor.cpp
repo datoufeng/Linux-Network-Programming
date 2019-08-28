775d43471da57a70be8444d0195f0b4b14ea2437
haifengMC <1745502795@qq.com>
2019-08-28 19:38:53 +0800

    epoll反应堆测试

--- 04多路io复用-poll-epoll-epoll反应堆/07epoll反应堆/Reactor.cpp
+++ 04多路io复用-poll-epoll-epoll反应堆/07epoll反应堆/Reactor.cpp
@@ -1,12 +1,86 @@
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+
 #include "Reactor.h"
 
+using namespace std;
 
-
-Reactor::Reactor()
+EpollReactor::EpollReactor(const int& size)
 {
+	cout << __FUNCTION__ << endl;
+	epollFd = epoll_create(size);
 }
 
 
-Reactor::~Reactor()
+EpollReactor::~EpollReactor()
 {
+	cout << __FUNCTION__ << endl;
+	close(epollFd);
 }
+
+EpollReactor::__Event * EpollReactor::checkEvent(const int & fd)
+{
+	cout << __FUNCTION__ << endl;
+	std::map<int, __Event>::iterator it;
+	if (eventMap.end() == (it = eventMap.find(fd)))return nullptr;
+	return &it->second;
+}
+
+
+int EpollReactor::addEvent(const int & fd, const int & events, void(*const & callback)(int fd, int events, void *arg))
+{
+	cout << __FUNCTION__ << endl;
+	if (eventMap.end() != eventMap.find(fd) || NULL == callback)
+		return -1;
+
+	eventMap[fd] = { fd, events, callback, NULL, 0 };
+
+	epoll_event ee;
+	ee.events = events;
+	ee.data.ptr = &eventMap[fd];
+
+	epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &ee);
+
+
+	return 0;
+}
+
+int EpollReactor::delEvent(const int & fd)
+{
+	cout << __FUNCTION__ << endl;
+	std::map<int, __Event>::iterator it;
+	if (eventMap.end() == (it = eventMap.find(fd))) return -1;
+
+	epoll_event ee;
+	ee.data.ptr = NULL;
+	ee.events = it->second.events;
+
+	epoll_ctl(epollFd, EPOLL_CTL_DEL, fd, &ee);
+
+	eventMap.erase(it);
+
+	return 0;
+}
+
+int EpollReactor::setEvent(const int & fd, const int & events, void(*const & callback)(int fd, int events, void *arg))
+{
+	cout << __FUNCTION__ << endl;
+	std::map<int, __Event>::iterator it;
+	if (eventMap.end() == (it = eventMap.find(fd)) || NULL == callback)
+		return -1;
+	
+	it->second.events = events;
+	it->second.callback = callback;
+
+	epoll_event ee;
+	ee.events = events;
+	ee.data.ptr = &eventMap[fd];
+
+	epoll_ctl(epollFd, EPOLL_CTL_MOD, fd, &ee);
+
+	return 0;
+}
+
--- 04多路io复用-poll-epoll-epoll反应堆/07epoll反应堆/Reactor.h
+++ 04多路io复用-poll-epoll-epoll反应堆/07epoll反应堆/Reactor.h
@@ -1,17 +1,35 @@
 #pragma once
-class Reactor
+#include <map>
+
+class EpollReactor
 {
 	struct __Event
 	{
-		int fdl;
+		int fd;
 		int events;
 		void(*callback)(int fd, int events, void* arg);
+		void* data;
+		int dataSize;
 	};
 public:
-	Reactor();
-	~Reactor();
+	EpollReactor(const int& size);
+	~EpollReactor();
 
+	__Event* checkEvent(const int& fd); 
+
+	int addEvent(
+		const int& fd,
+		const int& events,
+		void(*const& callback)(int fd, int events, void* arg));
+	int delEvent(const int& fd);
+	int setEvent(
+		const int& fd,
+		const int& events,
+		void(*const& callback)(int fd, int events, void* arg));
 private:
 	int epollFd;
+	std::map<int, __Event> eventMap;
+
+	
 };
 
